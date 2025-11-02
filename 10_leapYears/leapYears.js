2fa457839c0eef813ab63db261ba7c0f029344a5
WildForce112 <81035327+WildForce112@users.noreply.github.com>
2025-11-02 09:10:45 +0700

    Finished all excercises

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
+const leapYears = function(givenYear) {
+  const isYearDivisibleByFour = givenYear % 4 === 0;
+  const isCentury = givenYear % 100 === 0;
+  const isYearDivisibleByFourHundred = givenYear % 400 === 0;
 
+  if(isYearDivisibleByFour &&
+    (!isCentury || isYearDivisibleByFourHundred)) return true;
+  else return false;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
